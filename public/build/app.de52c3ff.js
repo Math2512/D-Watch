e7248f79d46b9b26743aaaa76a9000d5d768093c
Math2512 <mathieu.fontainemf@gmail.com>
2020-12-24 02:30:09 +0100

    ok

---
+++ public/build/app.de52c3ff.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["app"],{"1Wo5":function(o,n,t){"use strict";t.r(n);t("ldto");var c=t("EVdn");t("SYky"),t("vs8Z"),t("cFE2"),c(document).ready((function(){c('[data-toggle="popover"]').popover()})),c(".submit").click((function(){c("#wait-overlay").show()}))},ldto:function(o,n,t){}},[["1Wo5","runtime",0]]]);
