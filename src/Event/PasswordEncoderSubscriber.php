c55c34f7dd998613dc426de4592af2211522b1e2
Math2512 <mathieu.fontainemf@gmail.com>
2020-12-31 14:40:58 +0100

    ok

--- src/Event/PasswordEncoderSubscriber.php
+++ src/Event/PasswordEncoderSubscriber.php
@@ -2,11 +2,12 @@
 
 namespace App\Event;
 
-use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\User;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\Event\ViewEvent;
+use App\Entity\Article;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use ApiPlatform\Core\EventListener\EventPriorities;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class PasswordEncoderSubscriber implements EventSubscriberInterface
@@ -34,6 +35,29 @@ class PasswordEncoderSubscriber implements EventSubscriberInterface
         if($result instanceof User && $method === 'POST'){
             $hash = $this->encoder->encodePassword($result, $result->getPassword());
             $result->setPassword($hash);
+            
+        }
+
+        if($result instanceof Article && $method === 'POST'){
+            
+            $JsonResponse = file_get_contents("https://unfurl.io/api/v2/preview?api_token=xC3jXNC2vXfiOt6bpZcq6cPs3ncJAZYxIX7YQfgGrXS9HbhJRVcARc0ZbTJP&url=".$result->getLink());
+            $response = json_decode($JsonResponse, true);
+
+            if(isset($response['open_graph']['images'][0]['url'])){
+                $image = $response['open_graph']['images'][0]['url'];
+                
+                if($result->getTitle() == '')
+                    isset($response['title']) ? $title = $response['title']: 'Titre';
+                else
+                    $title = $result->getTitle();
+
+            }else{
+                $image= null;
+                $title = "Titre";
+            }
+
+            $result->setImgUrl($image);
+            $result->setTitle($title);
             
         }
     }
